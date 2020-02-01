9c3804269abd7f08616112ec35ccb99ab028c36c
Caio Nilton Lemos Barroso <caioniltonlemosbarroso@gmail.com>
2020-02-01 18:06:18 -0300

    bug fix

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export {CheckBox} from './CheckBox';
